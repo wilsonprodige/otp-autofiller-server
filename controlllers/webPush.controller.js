8f48480b9a54762a17c5b8be7c60ce513c896e4c
WILSON-Hardpro <wilsonzeugang.com>
2025-07-01 09:07:06 +0100

    quick updates

--- controlllers/webPush.controller.js
+++ controlllers/webPush.controller.js
@@ -4,12 +4,13 @@ const WebPushService = require('../services/webPushService')
 
 const subscribe = async (req, res, next) => {
     try {
+      //console.log('incoming request--->',req);
         const { subscription } = req.body;
         await WebPushService.saveSubscription(req.user.id, subscription);
-        res.status(201).json({ success: true });
+        return res.status(201).json({ success: true });
       } catch (error) {
         console.error('Subscription error:', error);
-        res.status(500).json({ error: 'Failed to save subscription' });
+        return res.status(500).json({ error: 'Failed to save subscription' });
       }
 };
 
