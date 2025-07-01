c1e495f6fc8ae15661460e86dac7e2bbcc5424b4
WILSON-Hardpro <wilsonzeugang.com>
2025-07-01 09:07:29 +0100

    push notification routes

--- routes/pushNotification.routes.js
+++ routes/pushNotification.routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const gmailController = require('../controlllers/gmail.controller.js');
 const WebPushContoller = require('../controlllers/webPush.controller');
 
-const auth = require('../middlewares/auth.middleware')
+const {auth} = require('../middlewares/auth.middleware')
 
 router.post('/subscribe',auth, WebPushContoller.subscribe);
+//auth
 
 router.post('/gmail', gmailController.pushNotificationCallback);
 
